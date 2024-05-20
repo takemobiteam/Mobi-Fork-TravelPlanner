6f9b7f78f417e1097fe46a98ea11ce64ff019074
Yuening Zhang <yuening@takemobi.com>
2024-05-20 09:51:26 -0400

    Add basic output plan conversion

--- mobi-data/convert_problem.py
+++ mobi-data/convert_problem.py
@@ -2,6 +2,7 @@ import json
 from definitions import *
 from serializer import CustomEncoder
 from datetime import datetime, timedelta
+from enum import Enum
 
 DAY_DURATION = 24 * 60 * 60 # 1 day
 
@@ -19,7 +20,7 @@ LUNCH_END_TIME = 14 * 60 * 60 # 2:00 PM
 DINNER_START_TIME = 18 * 60 * 60 # 6:00 PM
 DINNER_END_TIME = 20 * 60 * 60 # 8:00 PM
 
-HOTEL_BASE_DURATION = 8 * 60 * 60 # 8 hour
+HOTEL_BASE_DURATION = 1 * 60 * 60 # 1 hour
 HOTEL_UTILITY_SCALING = 20 # Scaled by ratings
 HOTEL_VISIT_UTILITY = 100
 HOTEL_CHECKIN_TIME = 15 * 60 * 60 # 3:00 PM
@@ -35,6 +36,15 @@ FAKE_LAT = 0.0
 FAKE_LON = 0.0
 DEFAULT_LAT = 42.2773242
 DEFAULT_LON = -89.08814249
+
+# POI type as an enum
+class POIType(Enum):
+    RESTAURANT = 1
+    ATTRACTION = 2
+    ACCOMMODATION = 3
+    FLIGHT = 4
+    SELF_DRIVING = 5
+    TAXI = 6
 
 def load_jsonl(file_path):
     all_data = []
@@ -85,7 +95,8 @@ def convert_problem(data):
         budget.name = 'Budget'
         budget.upperbound = data['budget']
         budget.ub_relaxation_cost = [0.0, 1.0e20]
-        problem.add_global_constraint(budget)
+        # TODO: add budget constraint back
+        #  problem.add_global_constraint(budget)
     else:
         budget = None
 
@@ -550,14 +561,127 @@ def get_restaurant_time_windows(meal, date):
     time_windows.append([start_time, end_time])
     return time_windows
 
+def parse_travel_plan(input_json, poi_type_map):
+    # Load the json file
+    with open(input_json, 'r') as f:
+        data = json.load(f)
+
+    plan = []
+    days = 1
+    id2location = dict()
+    prev_city = None
+
+    for route in data['routes']:
+        day_plan = {
+            "days": days,
+            "current_city": "-",
+            "transportation": "-",
+            "breakfast": "-",
+            "attraction": "-",
+            "lunch": "-",
+            "dinner": "-",
+            "accommodation": "-"
+        }
+        days += 1
+        # Obtain the current city, transportation, accommodation, breakfast, lunch, dinner, and attractions for the day
+        for i in range(len(route)):
+            segment = route[i]
+            name = segment['name'] if 'name' in segment else "-"
+            if name in poi_type_map:
+                if poi_type_map[name] == POIType.RESTAURANT:
+                    # check the start time of the segment to determine if it is breakfast, lunch, or dinner
+                    start_time = segment['startTimeRange'][0]
+                    if start_time >= BREAKFAST_START_TIME and start_time < BREAKFAST_END_TIME:
+                        day_plan["breakfast"] = name
+                    elif start_time >= LUNCH_START_TIME and start_time < LUNCH_END_TIME:
+                        day_plan["lunch"] = name
+                    elif start_time >= DINNER_START_TIME and start_time < DINNER_END_TIME:
+                        day_plan["dinner"] = name
+                elif poi_type_map[name] == POIType.ATTRACTION:
+                    if day_plan["attraction"] == "-":
+                        day_plan["attraction"] = name
+                    else:
+                        day_plan["attraction"] += "; " + name
+                elif poi_type_map[name] == POIType.ACCOMMODATION:
+                    # Only hotel if last segment of the day
+                    if i == len(route) - 1:
+                        day_plan["accommodation"] = name
+                elif poi_type_map[name] == POIType.FLIGHT:
+                    day_plan["transportation"] = name
+                elif poi_type_map[name] == POIType.SELF_DRIVING:
+                    day_plan["transportation"] = "Self-driving"
+                    start_loc = get_location(segment['startLocation'], id2location)
+                    end_loc = get_location(segment['endLocation'], id2location)
+                    start_city = start_loc['name']
+                    end_city = end_loc['name']
+                    prev_city = end_city
+                    day_plan["current_city"] = "from " + start_city + " to " + end_city
+                elif poi_type_map[name] == POIType.TAXI:
+                    day_plan["transportation"] = "Taxi"
+                    start_loc = get_location(segment['startLocation'], id2location)
+                    end_loc = get_location(segment['endLocation'], id2location)
+                    start_city = start_loc['name']
+                    end_city = end_loc['name']
+                    prev_city = end_city
+                    day_plan["current_city"] = "from " + start_city + " to " + end_city
+            else:
+                get_location(segment['startLocation'], id2location)
+                get_location(segment['endLocation'], id2location)
+        if day_plan["current_city"] == "-" and prev_city is not None:
+            day_plan["current_city"] = prev_city
+        plan.append(day_plan)
+    return plan
+
+def get_location(segment_loc, id2location):
+    if isinstance(segment_loc, dict):
+        locationId = segment_loc["@id"]
+        id2location[locationId] = segment_loc
+        print("Added location: ", segment_loc)
+        return segment_loc
+    else:
+        if segment_loc not in id2location:
+            print("Location not found: ", segment_loc)
+        assert(segment_loc in id2location)
+        segment_loc = id2location[segment_loc]
+        return segment_loc
+
+def get_poi_type(data):
+    # From input json data, obtain the name of POI -> Type: restaurant, attraction, accommodation, flight, self-driving, taxi
+    poi_type_map = {}
+    info = data['structured_ref_info']
+    for i in range(len(info)):
+        if info[i]['Info Type'] == 'Attractions':
+            if info[i]['Number'] > 0:
+                for name in info[i]['Structured Content']['Name'].values():
+                    poi_type_map[name] = POIType.ATTRACTION
+        elif info[i]['Info Type'] == 'Restaurants':
+            if info[i]['Number'] > 0:
+                for name in info[i]['Structured Content']['Name'].values():
+                    poi_type_map[name] = POIType.RESTAURANT
+        elif info[i]['Info Type'] == 'Accommodations':
+            if info[i]['Number'] > 0:
+                for name in info[i]['Structured Content']['NAME'].values():
+                    poi_type_map[name] = POIType.ACCOMMODATION
+        elif info[i]['Info Type'] == 'Flight':
+            if info[i]['Number'] > 0:
+                for name in info[i]['Structured Content']['Flight Number'].values():
+                    poi_type_map[name] = POIType.FLIGHT
+    poi_type_map['self-driving'] = POIType.SELF_DRIVING
+    poi_type_map['taxi'] = POIType.TAXI
+    return poi_type_map
+
 
 if __name__ == '__main__':
     data = load_jsonl('converted_data/train_data_list.jsonl')
     #  for problem_data in data[:1]:
-    for problem_data in data[1:2]:
-    #  for problem_data in data[18:19]:
+    #  for problem_data in data[1:2]:
+    for problem_data in data[18:19]:
         converted_problem = convert_problem(problem_data)
         # Do something with the converted problem
         encoded_problem = json.dumps(converted_problem, cls=CustomEncoder, indent=2)
-        print(encoded_problem)
+        #  print(encoded_problem)
+
+        #  print(get_poi_type(problem_data))
+        output_filepath = "/Users/yuening/mobi/planner/tempSolutionABP.solution"
+        print(parse_travel_plan(output_filepath, get_poi_type(problem_data)))
 
